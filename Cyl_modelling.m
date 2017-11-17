beb0ba8da16bd58276e6e23ce2d0804b9d6304b2
jcarballeira <juancarballeiralopez@gmail.com>
2017-11-17 19:00:56 +0100

    updates

--- Cyl_modelling.m
+++ Cyl_modelling.m
@@ -4,25 +4,72 @@ clc
 
 path='/home/jcl/Matlab2017b/DE_cylinder_modelling';
 holes_dir=dir([path '/*.pcd']);
-holes=size(holes_dir,1);
-centers=csvread('centers.csv');
+holes=size(holes_dir,1)
 
+%Plot each hole cloud
 for i=1:holes
-center=centers(i,:)
-
+    
 cloudfile=strcat('hole', num2str(i));
-cloudfile=strcat(cloudfile, '.pcd')
+cloudfile=strcat(cloudfile, '.pcd');
 
 nube = pcread(cloudfile);
 pcshow(nube, 'MarkerSize', 25)
   xlabel('X')
   ylabel('Y')
   zlabel('Z')
-hold on
+hold on 
+end
 
+
+NP=input('\ \n Introduce population number: \n');
+if isempty(NP)
+    NP=100;   
+    fprintf(1,'\n \t Population 100 by default. \n');
+end
+
+NP=round(NP);
+fprintf(1,'\n Population size: %i \n',NP);
+
+% Genetic algorithm upper iterations limit
+iter_max=input('\ \n Introduzce the maximum number of iterations: \ \');
+iter_max=round(iter_max);
+if isempty(iter_max)
+    iter_max= 40;   
+    fprintf(1,'\n \t The default iterations are %d \n',iter_max);
+end
+
+%--------------------------------------------------------------------------
+% Different options for the GL algorithm can be selected via keyboard:
+%  - DE Core Options:
+%    1) Random Mutation, with Thresholding and Discarding (Default). 
+%    2) Basic version, Random Mutation, without Thresholding, Discarding.
+%    3) Mutation from Best candidate, with Thresholding and Discarding.
+%    4) Random Mutation, with Thresholding and Discarding, NP is
+%    drastically reduced (tracking) after convergence.
+version_de=input('\ \n Introduce the DE version that you want to apply: \n 1) Random Mutation, with Thresholding and Discarding. \n 2) Basic version, Random Mutation, without Thresholding, Discarding. \n 3) Mutation from Best candidate, with Thresholding and Discarding. \n 4) Random Mutation, with Thresholding and Discarding, NP reduced (tracking) after convergence. \n');
+if isempty(version_de),
+    version_de=1;   
+    fprintf(1,'\n \t Option 1 by default. \n');
+end
+
+centers=csvread('centers.csv')
+
+%Evaluate each hole cloud
+for i=1:holes
+    
+center=centers(i,:)
+
+cloudfile=strcat('hole', num2str(i));
+cloudfile=strcat(cloudfile, '.pcd')
+
+nube = pcread(cloudfile);    
 Points=nube.Count;
-fprintf(1,'\n Hole points considered: %i \n',Points);
 
+if Points > 10
+
+   
+
+fprintf(1,'\n Hole points considered: %i \n',Points);    
 xyz=nube.Location;
 xyz=reshape(xyz, [1 size(xyz,1)*size(xyz,2)]);
 xyz(isnan(xyz))=[];
@@ -46,42 +93,11 @@ CR=0.5;                     % Crossover constant
 
 % Population parameter boundaries,[x,y,z,u,v,w]: xyz center point, uvw axis vector
 %vector_min=[-1 -1 -1];
-vector_min=[-0.3 -0.3 0.6];
+vector_min=[-0.4 -0.4 0.6];
 %vector_max=[1 1 1 ];
-vector_max=[0.3 0.3 1];
+vector_max=[0.4 0.4 1];
 min=[center(1)-0.05, center(2)-0.05, ptCloud.ZLimits(1)-0.01, vector_min(1), vector_min(2), vector_min(3)] %minimum an maximum
 max=[center(1)+0.05, center(2)+0.05, ptCloud.ZLimits(1)+0.01, vector_max(1), vector_max(2), vector_max(3)]
-
-NP=input('\ \n Introduce population number: \n');
-if isempty(NP)
-    NP=100;   
-    fprintf(1,'\n \t Population 100 by default. \n');
-end
-
-NP=round(NP);
-fprintf(1,'\n Population size: %i \n',NP);
-
-% Genetic algorithm upper iterations limit
-iter_max=input('\ \n Introduzce the maximum number of iterations: \ \');
-iter_max=round(iter_max);
-if isempty(iter_max),
-    iter_max= 40;   
-    fprintf(1,'\n \t The default iterations are %d \n',iter_max);
-end
-
-%--------------------------------------------------------------------------
-% Different options for the GL algorithm can be selected via keyboard:
-%  - DE Core Options:
-%    1) Random Mutation, with Thresholding and Discarding (Default). 
-%    2) Basic version, Random Mutation, without Thresholding, Discarding.
-%    3) Mutation from Best candidate, with Thresholding and Discarding.
-%    4) Random Mutation, with Thresholding and Discarding, NP is
-%    drastically reduced (tracking) after convergence.
-version_de=input('\ \n Introduce the DE version that you want to apply: \n 1) Random Mutation, with Thresholding and Discarding. \n 2) Basic version, Random Mutation, without Thresholding, Discarding. \n 3) Mutation from Best candidate, with Thresholding and Discarding. \n 4) Random Mutation, with Thresholding and Discarding, NP reduced (tracking) after convergence. \n');
-if isempty(version_de),
-    version_de=1;   
-    fprintf(1,'\n \t Option 1 by default. \n');
-end
 
 N_SIMULATIONS=1;
 Solution.best_estimate=zeros(N_SIMULATIONS,D);
@@ -89,7 +105,6 @@ Solution.error=zeros(N_SIMULATIONS,1);
 
 for simul=1:N_SIMULATIONS
     
-%-----------------------------y a este señor quien le eligió para nada? demoque?---------------------------------------------
 % The initial population is randomly generated.
 population=initiate_pop(min,max,NP,D);
 
@@ -140,5 +155,8 @@ params=[Solution.best_estimate(1),Solution.best_estimate(2),Solution.best_estima
 model = cylinderModel(params);
 %quiver3(Solution.best_estimate(2),Solution.best_estimate(3),Solution.best_estimate(4),Solution.best_estimate(5),Solution.best_estimate(6),Solution.best_estimate(7))
 plot(model)
+else 
+fprintf(1,'\n More points needed to analyze the hole');    
+end
 end
 end
